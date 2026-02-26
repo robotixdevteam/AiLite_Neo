03408b786eaec45d377d8503c0edfd92bf24cca4
robotixdevteam <96867511+robotixdevteam@users.noreply.github.com>
2026-02-26 12:26:08 +0530

    Update TM_FACE.py

--- 18_Teachable_Machine_Face_Recognition/TM_FACE.py
+++ 18_Teachable_Machine_Face_Recognition/TM_FACE.py
@@ -4,21 +4,8 @@ import cv2  # Install opencv-python
 import numpy as np
 import requests
 
+host_bot=input("Enter brain block ip address : ")
 
-
-bot_num = input("Enter bot number : ")
-host_bot="192.168."+bot_num+".10"
-
-
-def get_ultrasonic_value():
-    ultrasonic_url = f"http://192.168.{bot_num}.10/?cmd=US"
-    response = requests.get(ultrasonic_url)
-    if response.status_code == 200:
-        try:
-            return int(response.text)
-        except ValueError:
-            return None
-    return None 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
@@ -32,11 +19,6 @@ class_names = open("TM_FACE_labels.txt", "r").readlines()
 camera = cv2.VideoCapture(0)
 
 while True:
-    
-    ultrasonic_value = get_ultrasonic_value()
-        
-    if ultrasonic_value is not None and ultrasonic_value < 15:
-        requests.get(f"http://{host_bot}/?cmd=b(200)")
     
     # Grab the webcamera's image.
     ret, image = camera.read()
